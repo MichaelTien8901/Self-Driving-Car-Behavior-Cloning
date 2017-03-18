ccd6c50b1198814280f994c302d83736dd9dea8d
YMSGROG\Michael Tien <mtien888@gmail.com>
2017-03-17 17:49:08 -0700

    preprocessing image with greyscale and histogram balanced.

--- model.py
+++ model.py
@@ -3,6 +3,10 @@ import cv2
 import csv
 import numpy as np
 
+def preprocessing_image(image):
+    out_image = cv2.equalizeHist(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY))
+    return out_image
+
 lines = []
 with open('data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
@@ -14,8 +18,7 @@ measurements = []
 ## for windows, path splitter is \
 #PATH_SEPARATOR = '\\'
 PATH_SEPARATOR = '/'
-#STEERING_CORRECTION = 0.5
-STEERING_CORRECTION = 0.8
+STEERING_CORRECTION = 0.1
 for line in lines:
     steering_center = float(line[3])
     steering_left = steering_center + STEERING_CORRECTION
@@ -25,9 +28,9 @@ for line in lines:
     left_filename = line[1].split(PATH_SEPARATOR)[-1]
     right_filename = line[2].split(PATH_SEPARATOR)[-1]
     current_path = 'data' + PATH_SEPARATOR + 'IMG' + PATH_SEPARATOR
-    center_image = cv2.imread(current_path + center_filename)
-    left_image = cv2.imread(current_path + left_filename)
-    right_image = cv2.imread(current_path + right_filename)
+    center_image = preprocessing_image(cv2.imread(current_path + center_filename))
+    left_image = preprocessing_image(cv2.imread(current_path + left_filename))
+    right_image = preprocessing_image(cv2.imread(current_path + right_filename))
     images.extend([center_image, left_image, right_image])
     measurements.extend([steering_center, steering_left, steering_right])
 #    images.append(center_image)
@@ -40,7 +43,7 @@ for image, measurement in zip(images, measurements):
     augmented_images.append(cv2.flip(image, 1))
     augmented_measurements.append(measurement * -1.0)
 
-X_train = np.array(augmented_images)
+X_train = np.expand_dims(np.array(augmented_images), axis=3) ## for gray
 y_train = np.array(augmented_measurements)
 
 from keras.models import Sequential
@@ -51,9 +54,10 @@ from keras.layers.convolutional import Convolution2D
 #dropout_prob = 0.5
 
 model = Sequential()
-model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+#model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 3)))
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160, 320, 1)))
 ### cropping
-model.add(Cropping2D(cropping=((72, 25), (0,0))))
+model.add(Cropping2D(cropping=((60, 25), (0,0))))
 
 model.add(Convolution2D(24, 5, 5, subsample=(2,2), activation="relu"))
 model.add(Convolution2D(36, 5, 5, subsample=(2,2), activation="relu"))
@@ -69,5 +73,5 @@ model.add(Dense(1))
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
 
-model.save('model.h5')
+model.save('model_hist.h5')
 print("model saved!")
